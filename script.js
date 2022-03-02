84c6e743a53fde6a1ba749528cae986dbf48b885
Ríchard Lucas Cordeiro Moreira <richardlcmoreira@gmail.com>
2022-03-02 00:27:52 -0300

    added tasklist // small improvments and ids changes

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-let counter = 0;
-
-//hello / goodbye
+//hello => goodbye
 function hello () {
 
     if (document.querySelector('h1').innerHTML === 'Hello') {
@@ -12,6 +10,8 @@ function hello () {
 };
 
 //counter
+let counter = 0;
+
 function count() {
     
     counter++;
@@ -23,23 +23,60 @@ function count() {
 };
 
 //play the script after the DOM's download
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
 
     //counter
     document.querySelector(".button2").onclick = count;
 
-    //name form
-    document.querySelector('form').onsubmit = () => {
-        const name = document.querySelector('#name').value;
-        alert(`Hello, ${name}!`);
-    };
-
     //color buttons
     document.querySelectorAll(".colorbutton").forEach(button => {
-        button.onclick = function() {
+        button.onclick = () => {
             document.querySelector('#color').style.color = button.dataset.color;
         }
 
     });
+
+    //color dropdown
+    document.querySelector("select").onchange = function () {
+        document.querySelector('#color').style.color = this.value;
+    }
+
+    //name form
+    document.querySelector('#submit').disabled = true;
+
+    document.querySelector('#name').onkeyup = () => { 
+        document.querySelector('#submit').disabled = false
+    }
+
+    document.querySelector('.form4').onsubmit = () => {
+    const name = document.querySelector('#name').value;
+    alert(`Hello, ${name}!`);
+    };
+
+
+
+
+
+    
+    //tasks
+    document.querySelector('#submit2').disabled = true;
+
+    document.querySelector('#task').onkeyup = () => { 
+        document.querySelector('#submit2').disabled = false
+    }
+
+    document.querySelector('.form5').onsubmit = () => {
+        const task = document.querySelector('#task').value;
+
+        const li = document.createElement('li');
+        li.innerHTML = task;
+            
+        document.querySelector('#tasks').append(li);
+
+        document.querySelector('#task').value = '';
+        
+
+        return false
+    }
 
 });
